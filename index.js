f82e13c83f9c23e6b56dbf0a073ab5eb3a0b235b
keepitclassick <keepitclassick@gmail.com>
2022-02-24 15:41:58 -0500

    add mouse move event listener

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const canvas = document.createElement("canvas1");
+const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+
+//handle mouse
+let mouse = {
+  x: null,
+  y: null,
+  radius: 150,
+};
+
+window.addEventListener("mousemove", function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  console.log(mouse.x, mouse.y);
+});
