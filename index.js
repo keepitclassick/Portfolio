a0518070b3e2ed025e03b09cba7ef24b0a723791
keepitclassick <keepitclassick@gmail.com>
2022-02-24 16:12:27 -0500

    create particle class

--- index.js
+++ index.js
@@ -14,5 +14,35 @@ let mouse = {
 window.addEventListener("mousemove", function (e) {
   mouse.x = e.x;
   mouse.y = e.y;
-  console.log(mouse.x, mouse.y);
+  // console.log(mouse.x, mouse.y);
 });
+
+ctx.fillStyle = "White";
+ctx.font = "30px Verdana";
+ctx.fillText("Kayla", 0, 30);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x + 100;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 30 + 1;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function init() {
+  particleArray = [];
+  particleArray.push(new Particle(50, 50));
+}
+init();
+console.log(particleArray);
