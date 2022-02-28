622c0f3d04a755f4d1ae1b9b258901dc1fb84858
keepitclassick <keepitclassick@gmail.com>
2022-02-28 14:44:40 -0500

    add cards with project details

--- particles.js
+++ particles.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 let particleArray = [];
 
 //handle mouse
