a9757e8550c449f51c4b7640f778cdf167165a97
keepitclassick <keepitclassick@gmail.com>
2022-03-08 14:12:00 -0500

    add event listeners to contact form

---
+++ contact.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fields.firstName = document.getElementById("fname");
+  fields.email = document.getElementById("email");
+  fields.message = document.getElementById("message");
+});
