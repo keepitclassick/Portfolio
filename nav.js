7ff37e6153b594678a2b32015604b5b6faa7770d
keepitclassick <keepitclassick@gmail.com>
2022-03-11 18:08:14 -0500

    add custom hamburger menu

--- nav.js
+++ nav.js
@@ -1,9 +1,8 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+document.ready(function () {
+  $.each($("#navbar").find("li"), function () {
+    $(this).toggleClass(
+      "active",
+      window.location.pathname.indexOf($(this).find("a").attr("href")) > -1
+    );
+  });
+});
