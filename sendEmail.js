98fb51a5ed40f184133cbabd74cc4d072eb556f3
keepitclassick <keepitclassick@gmail.com>
2022-03-21 18:06:48 -0400

    replace form function

--- sendEmail.js
+++ sendEmail.js
@@ -2,10 +2,16 @@ function sendEmail() {
   Email.send({
     Host: "smtp.gmail.com",
     Username: "keepitclassick@gmail.com",
-    Password: process.env.PASS,
+    Password: "Toodles52",
     To: "keepitclassick@gmail.com",
     From: document.getElementById("email").value,
     Subject: "New Website Inquiry",
-    Body: document.getElementById("message").value,
+    Body:
+      "name:" +
+      document.getElementById("fname").value +
+      "<br> email:" +
+      document.getElementById("email").value +
+      "<br>message:" +
+      document.getElementById("message").value,
   }).then((message) => alert(message));
 }
